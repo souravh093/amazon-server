eb27e84065f1cd92c60a681908a617a783e7074b
Sourav halder <souravh093@gmail.com>
2023-05-15 19:26:10 +0600

    add mongodb in my server and show data using reflktor

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 const express = require("express");
 const cors = require("cors");
-require('dotenv').config()
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 
-const app = express()
+const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Amazon server running')
-})
+app.get("/", (req, res) => {
+  res.send("Amazon server running");
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ukmkwhb.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const productCollection = client.db("amazonDB").collection("products");
+
+    app.get('/products', async(req, res) => {
+        const result = await productCollection.find().toArray();
+        res.send(result)
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
-    console.log(`Amazon server is running on port ${port}`)
-})
+  console.log(`Amazon server is running on port ${port}`);
+});
