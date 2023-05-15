0fac9f013e455441c8df1c794ecdd321e27ca46f
Sourav halder <souravh093@gmail.com>
2023-05-15 15:26:04 +0600

    amazon server running

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Amazon server running')
+})
+
+app.listen(port, () => {
+    console.log(`Amazon server is running on port ${port}`)
+})
